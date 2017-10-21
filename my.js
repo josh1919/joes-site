d9238d0b0f845b11512e0435b42886c2981d66d8
Joshy <josh@joshcruz.net>
2017-10-21 10:48:17 -0700

    bug fix: lesson only rotating when scroll past. Now fixed to rotate when scroll past center. Also delete useless and commented out code

--- my.js
+++ my.js
@@ -15,49 +15,18 @@ function debounce(func, wait = 20, immediate = true) {
 
 const myLessons = document.querySelectorAll('.lesson');
 
-// function lessonRotate(e) {
-//   myLessons.forEach(lesson => {
-//     const squareCenter = (window.scrollY + window.innerHeight);
-//     const squareBottom = lesson.offsetTop + lesson.height;
-//     const isHalfShown = squareCenter < lesson.offsetTop;
-//     const isNotScrolledPast = window.scrollY < squareBottom;
-//     //const myObject = [squareCenter, squareBottom, isHalfShown, isNotScrolledPast]
-//     if((window.scrollY + window.innerHeight) > squareCenter) {
-//       lesson.classList.add('straighten');
-      
-//      } else {
-//        console.log(squareCenter)
-//       lesson.classList.remove('straighten');
-//     }
-//   })
-// }
-
-
 function lessonRotate(e) {
   myLessons.forEach(lesson => {
-    const lessonCenterAA = (window.scrollY + window.innerHeight) - lesson.clientHeight/2;
-    const squareCenterOnUp = (window.scrollY) + lesson.clientHeight / 2;
     const lessonCenter = lesson.offsetTop + lesson.clientHeight/2;
-    const isHalfShown = lessonCenter < lesson.offsetTop;
-    const isNotScrolledPast = window.scrollY < lessonCenter;
+    const lessonfromBottom = window.scrollY + window.innerHeight;
 
-    const lessonfromBottom = (window.scrollY + window.innerHeight) - lesson.clientHeight/2
-
-  
-
-     // lessonoffset + lessonheight/2 > window.scrollY 
-
-    //const myObject = [squareCenter, squareBottom, isHalfShown, isNotScrolledPast]
     if(lessonCenter > window.scrollY && lessonCenter < lessonfromBottom) {
       lesson.classList.add('straighten');
-      console.log(lesson.offsetTop)
+      console.log("lesson.offsetTop = " + lesson.offsetTop + " lessonCenter = " + lessonCenter);
      } else {
        console.log(lesson.offsetTop)
       lesson.classList.remove('straighten');
     }
-
-    // if(squareBOttom)
-
   })
 }
 
